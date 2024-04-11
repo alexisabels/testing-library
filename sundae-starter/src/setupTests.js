487bd77fad0e19aae0562f8185d7faa00e97b767
alexisabels <isabelsanchezalex@gmail.com>
2024-04-11 10:41:29 +0200

    Code and test scoop options images from server

--- sundae-starter/src/setupTests.js
+++ sundae-starter/src/setupTests.js
@@ -1,1 +1,14 @@
 import "@testing-library/jest-dom";
+
+import { beforeAll, afterEach, afterAll } from "vitest";
+import { server } from "./mocks/server";
+
+// establish API mocking (do before all tests)
+beforeAll(() => server.listen());
+
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests
+afterEach(() => server.resetHandlers());
+
+// Clean server up after the tests are finished
+afterAll(() => server.close());
--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+import { kebabCaseToTitleCase } from "./helpers";
 function App() {
+  const [disabled, setDisabled] = useState(false);
+  const [buttonColor, setButtonColor] = useState("medium-violet-red");
+  const newButtonColor = buttonColor === "medium-violet-red" ? "midnight-blue" : "medium-violet-red";
+  const nextColorTitleCase = kebabCaseToTitleCase(newButtonColor)
+  const className = disabled ? "gray" : buttonColor;
   return (
     <div>
-      <h1>I'm gonna learn React Testing Library</h1>
+      <button className={className} disabled={disabled} onClick={() => setButtonColor(newButtonColor)}>
+        Change to {nextColorTitleCase}
+      </button>
+      <br />
+      <input type="checkbox" id="disable-button-checkbox" defaultChecked={false} onChange={(e) => setDisabled(e.target.checked)}/>
+      <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
 }
